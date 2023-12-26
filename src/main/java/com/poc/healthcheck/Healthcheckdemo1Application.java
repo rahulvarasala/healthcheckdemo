1fc1c4279aa0834eb7b3192ae1bcc7d092599b76
Rahul <vrslrahul@gmail.com>
2023-12-26 17:58:04 +0530

    New code

---
+++ src/main/java/com/poc/healthcheck/Healthcheckdemo1Application.java
@@ -0,0 +1,13 @@
+package com.poc.healthcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Healthcheckdemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Healthcheckdemo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/poc/healthcheck/Healthcheckdemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.healthcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Healthcheckdemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
